317328d75064b1192afffaba2947663ab225d873
rodgomesc <rodgomesc@gmail.com>
2020-04-26 22:56:38 -0300

    style(login screen): finish login scren layout and styles

--- src/containers/Signin/index.tsx
+++ src/containers/Signin/index.tsx
@@ -1,29 +1,44 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
+import { StatusBar } from 'react-native';
 import loginBackground from '../../assets/loginBackground.png';
 import logo from '../../assets/Logo.png';
 import { Neomorph } from 'react-native-neomorph-shadows';
 
-import { Overlay, Container, Avatar, AvatarWrapper } from './styles';
+import {
+  Overlay,
+  Container,
+  Avatar,
+  LoginButton,
+  LoginButtonText,
+  AppTitle,
+} from './styles';
 
 export default () => (
-  <Overlay source={loginBackground}>
-    <Container>
-      <Neomorph
-        style={{
-          shadowRadius: 4,
-          shadowOffset: { width: 4, height: 4 },
-          width: 192,
-          height: 192,
-          borderRadius: 96,
-          backgroundColor: 'rgba(204,0,255,0.1)',
-          marginTop: 60,
-          marginLeft: 'auto',
-          marginRight: 'auto',
-        }}
-      >
-        <Avatar source={logo} />
-      </Neomorph>
-    </Container>
-  </Overlay>
+  <>
+    <StatusBar hidden />
+    <Overlay source={loginBackground}>
+      <Container>
+        <Neomorph
+          style={{
+            shadowRadius: 4,
+            shadowOffset: { width: 4, height: 4 },
+            width: 192,
+            height: 192,
+            borderRadius: 96,
+            backgroundColor: 'rgba(180,0,255,0.1)',
+            marginTop: 160,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
+        >
+          <Avatar source={logo} />
+        </Neomorph>
+        <AppTitle>jobs finder</AppTitle>
+        <LoginButton>
+          <LoginButtonText>Logar utilizando Github</LoginButtonText>
+        </LoginButton>
+      </Container>
+    </Overlay>
+  </>
 );
--- src/containers/Signin/styles.ts
+++ src/containers/Signin/styles.ts
@@ -1,24 +1,41 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled(LinearGradient).attrs({
-  colors: ['rgba(34,2,234,.8)', 'rgba(204,0,255,.7)'],
-})`
-  flex: 1;
-`;
-
 export const Overlay = styled.ImageBackground`
   flex: 1;
 `;
 
-export const AvatarWrapper = styled.View`
-  width: 192px;
-  height: 192px;
-  border-radius: 96px;
-  margin: 60px auto;
-  background: rgba(255, 255, 255, 0.2);
+export const Container = styled(LinearGradient).attrs({
+  colors: ['rgba(34,2,234,.8)', 'rgba(204,0,255,.7)'],
+})`
+  flex: 1;
+  align-items: center;
 `;
 
 export const Avatar = styled.Image`
+  margin: auto auto;
+`;
+
+export const AppTitle = styled.Text`
+  text-transform: uppercase;
+  color: #fff;
+  letter-spacing: 1px;
+  margin-top: 22px;
+  margin-bottom: 22px;
+  font-size: 28px;
+  font-weight: bold;
+`;
+
+export const LoginButton = styled.TouchableOpacity`
+  height: 64px;
+  width: 90%;
+  background: #591dc6;
+  border-radius: 10px;
+  elevation: 15;
+`;
+
+export const LoginButtonText = styled.Text`
+  color: #fff;
+  font-size: 18px;
   margin: auto auto;
 `;
