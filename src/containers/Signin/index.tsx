66fca8a13e616c28a91cd7d9481ae3214d885db0
rodgomesc <rodgomesc@gmail.com>
2020-04-26 21:24:23 -0300

    chore(login): initial implementation for login screen

--- src/containers/Signin/index.tsx
+++ src/containers/Signin/index.tsx
@@ -1,10 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { Text } from 'react-native';
+import loginBackground from '../../assets/loginBackground.png';
+import logo from '../../assets/Logo.png';
+import { Neomorph } from 'react-native-neomorph-shadows';
 
-import { Container } from './styles';
+import { Overlay, Container, Avatar, AvatarWrapper } from './styles';
 
 export default () => (
-  <Container>
-    <Text>hello from Signin</Text>
-  </Container>
+  <Overlay source={loginBackground}>
+    <Container>
+      <Neomorph
+        style={{
+          shadowRadius: 4,
+          shadowOffset: { width: 4, height: 4 },
+          width: 192,
+          height: 192,
+          borderRadius: 96,
+          backgroundColor: 'rgba(204,0,255,0.1)',
+          marginTop: 60,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+        }}
+      >
+        <Avatar source={logo} />
+      </Neomorph>
+    </Container>
+  </Overlay>
 );
--- src/containers/Signin/styles.ts
+++ src/containers/Signin/styles.ts
@@ -1,5 +1,24 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
 
-export const Container = styled.View`
+export const Container = styled(LinearGradient).attrs({
+  colors: ['rgba(34,2,234,.8)', 'rgba(204,0,255,.7)'],
+})`
   flex: 1;
+`;
+
+export const Overlay = styled.ImageBackground`
+  flex: 1;
+`;
+
+export const AvatarWrapper = styled.View`
+  width: 192px;
+  height: 192px;
+  border-radius: 96px;
+  margin: 60px auto;
+  background: rgba(255, 255, 255, 0.2);
+`;
+
+export const Avatar = styled.Image`
+  margin: auto auto;
 `;
